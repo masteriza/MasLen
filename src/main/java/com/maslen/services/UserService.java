c3ff303d1628f9a44a20a335256f1a35ef847a4e
nasteriza <masteriza@ukr.net>
2017-08-14 10:21:22 +0300

    security in developing

--- src/main/java/com/maslen/services/UserService.java
+++ src/main/java/com/maslen/services/UserService.java
@@ -1,4 +1,12 @@
 package com.maslen.services;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
