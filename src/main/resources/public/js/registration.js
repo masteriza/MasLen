8a9d0a64595f407ead9ea0212695227be07eb3b1
nasteriza <masteriza@ukr.net>
2017-08-22 08:54:15 +0300

    security in developing

--- src/main/resources/public/js/registration.js
+++ src/main/resources/public/js/registration.js
@@ -1,17 +1,33 @@
 $(document).ready(function () {
 
-    $('#btSingUp').click(function () {
-        var user = {}
+    $('#btnRegistration').click(function () {
+
+        var birthday_Day = $("#birthday_Day option:selected").val();
+        var birthday_Month = $("#birthday_Month option:selected").val();
+        var birthday_Year = $("#birthday_Year option:selected").val();
+
+
+        var user = {};
         user["userId"] = 0;
         user["lastName"] = $("#lastName").val();
         user["firstName"] = $("#firstName").val();
         user["middleName"] = $("#middleName").val();
-        user["birthday"] = $("#birthday").val();
+        user["birthday"] = new Date(Date.UTC(birthday_Year, birthday_Month, birthday_Day));
 
         user["email"] = $("#email").val();
         user["rawPassword"] = $("#rawPassword").val();
         user["repeatRawPassword"] = $("#repeatRawPassword").val();
         user["phone"] = $('#phone').val();
+
+        if (( form.genderRadios[0].checked == false ) && ( form.genderRadios[1].checked == false )) {
+            // alert("Please choose your Gender: Male or Female");
+            user["gender"] = "X";
+
+        } else {
+            user["gender"] = $('input[name=genderRadios]:checked').val();
+        }
+        //user["gender"] = $('input[name=genderRadios]:checked').val();
+        user["agree"] = document.getElementById("agree").checked;
         user["status"] = '';
 
         $.ajax({
