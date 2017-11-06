6965d2fc6dd6a8821fad220c79fa71306dcd02cb
nasteriza <masteriza@ukr.net>
2017-11-07 01:51:52 +0200

    -

--- src/main/java/com/maslen/dao/DriverDaoImpl.java
+++ src/main/java/com/maslen/dao/DriverDaoImpl.java
@@ -43,10 +43,51 @@ public class DriverDaoImpl implements DriverDao {
 
     @Override
     public List<Route> getAllRoute() {
-        List<Route> routes = currentSession().createQuery("from Route r join fetch r.routePoints").list();
-//        List<Route> routez = query.list();
-
+        List<Route> routes = currentSession().createQuery("select distinct r from Route r inner join fetch r.routePoints ", Route.class).list();
+        //List<Route> routes = currentSession().createQuery("from Route", Route.class).list();
+//        for (Route route : routes) {
+//
+//            for (RoutePoint routePoint : route.getRoutePoints()) {
+//                System.out.println(routePoint);
+//            }
+//
+//
+//        }
         System.out.println(routes);
         return routes;
     }
+
+
+//----------
+
+
+//    List<Person> personList = session.createQuery("FROM Person").list();
+//
+//
+//                for(
+//    Iterator iterator = personList.iterator();
+//    iterator.hasNext();
+//    )
+//
+//    {
+//        Person person = (Person) iterator.next();
+//        Set<Phone> ph = new HashSet<Phone>();
+//        ph = person.getPersonPhoneNumbers();
+//        for (Phone p : ph) {
+//            System.out.println("***************************");
+//            System.out.println(person.getName());
+//            System.out.println(" Phone Type: " + p.getPhonetype());
+//            System.out.println(" Phone Number: " + p.getContactnumber());
+//            System.out.println("***************************");
+//        }
+//    }
+
+
+//----------
 }
+
+
+
+
+
+
